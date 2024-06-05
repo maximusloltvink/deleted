20ff076f4a142f251f7bfc34e47e6a42aef2dcfe
maximusloltvink <141337353+maximusloltvink@users.noreply.github.com>
2024-06-05 07:43:05 +0300

    deleted soft

---
+++ Deleted.bat
@@ -0,0 +1,5 @@
+del C:\Windows\Prefetch\LUMINAR.EXE-AEBAD63A.pf
+reg delete HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\Shell\BagMRU
+reg delete HKEY_USERS\S-1-5-21-3234986558-81923781-4205828133-1001\Software\Microsoft\Windows\CurrentVersion\Explorer\FeatureUsage\AppSwitched
+reg delete HKEY_USERS\S-1-5-21-3234986558-81923781-4205828133-1001\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Compatibility Assistant\Store
+reg delete HKEY_USERS\S-1-5-21-3234986558-81923781-4205828133-1001\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\MuiCache
